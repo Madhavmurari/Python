ea0409feef9ddba8e6e3908b8e4fafee9987a5c1
Madhav Murari <143632752+Madhavmurari@users.noreply.github.com>
2024-06-22 13:35:28 +0530

    Time: 557 ms (14.91%), Space: 158 MB (29.48%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        cashe={}
+
+        def dfs(i,j):
+            if j==len(t):
+                return 1
+            if i==len(s):
+                return 0
+            if (i,j) in cashe:
+                return cashe[(i,j)]
+            if s[i]==t[j]:
+                cashe[(i,j)] = dfs(i+1,j+1) + dfs(i+1,j)
+            else:
+                cashe[(i,j)] = dfs(i+1,j)
+            return cashe[(i,j)]
+        return dfs(0,0)
