eb9fb52973adb8ffce2c90248f2e879c814fbaf1
Madhav Murari <143632752+Madhavmurari@users.noreply.github.com>
2024-07-02 17:52:42 +0530

    Time: 40 ms (30.89%), Space: 16.5 MB (36.86%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,10 +1,10 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        binary_str = bin(n)[2:].zfill(32)
-        # Reverse the binary string
-        reversed_str = binary_str[::-1]
-        # Convert the reversed binary string back to an integer
-        return int(reversed_str, 2)
+        # binary_str = bin(n)[2:].zfill(32)
+        # # Reverse the binary string
+        # reversed_str = binary_str[::-1]
+        # # Convert the reversed binary string back to an integer
+        # return int(reversed_str, 2)
 
         res=0
         for i in range(32):
