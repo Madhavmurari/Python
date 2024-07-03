e536693c9a775c74250f46f937841dbb346c27bf
Madhav Murari <143632752+Madhavmurari@users.noreply.github.com>
2024-07-03 13:22:35 +0530

    Time: 42 ms (8.69%), Space: 16.5 MB (65.83%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.py
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -7,9 +7,9 @@ class Solution:
         # works both as while loop and single value check 
         while (b & mask) > 0:
             
-            carry = ( a & b ) 
+            carry = ( a & b )<<1 
             a = (a ^ b) 
-            b = carry <<1
+            b = carry 
         
         # handles overflow
         return (a & mask) if b > 0 else a
