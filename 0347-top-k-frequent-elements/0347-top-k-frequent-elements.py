cc1ce11a496e6e489ee7c04efcb0b61563f0d004
Madhav Murari <143632752+Madhavmurari@users.noreply.github.com>
2024-08-23 16:32:44 +0530

    Time: 85 ms (76.62%), Space: 21.2 MB (65.21%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,18 +1,35 @@
+# class Solution:
+#     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+#         mapp={}
+#         freq=[[] for i in range(len(nums)+1)]
+
+#         for n in nums:
+#             mapp[n]=1+mapp.get(n,0)
+
+#         for n,c in mapp.items():
+#             freq[c].append(n)
+        
+#         res=[]
+
+#         for i in range(len(nums),0,-1):
+#             for n in freq[i]:
+#                 res.append(n)
+#                 if len(res)==k:
+#                     return res
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        mapp={}
-        freq=[[] for i in range(len(nums)+1)]
+        counter={}
+        heap=[]
 
         for n in nums:
-            mapp[n]=1+mapp.get(n,0)
+            counter[n]=1+counter.get(n,0)
 
-        for n,c in mapp.items():
-            freq[c].append(n)
+        for key,val in counter.items():
+            heapq.heappush(heap,(-val,key))
         
         res=[]
+        while len(res)<k:
+            res.append(heapq.heappop(heap)[1])
+        return res
 
-        for i in range(len(nums),0,-1):
-            for n in freq[i]:
-                res.append(n)
-                if len(res)==k:
-                    return res
