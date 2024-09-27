d0d08e45ef65e34cdf138a1175d7abf3de2ae00a
Madhav Murari <143632752+Madhavmurari@users.noreply.github.com>
2024-09-27 10:31:26 +0530

    Time: 1039 ms (52.15%), Space: 56.1 MB (26.49%) - LeetHub

---
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.py
@@ -0,0 +1,26 @@
+class Solution:
+    def cherryPickup(self, grid: List[List[int]]) -> int:
+
+        row,col=len(grid),len(grid[0])
+        cache={}
+
+        def dfs(r,c1,c2):
+            if (r,c1,c2) in cache:
+                return cache[(r,c1,c2)]
+            if c1==c2 or min(c1,c2)<0 or max(c1,c2)==col:
+                return 0 
+            if r==row-1:
+                return grid[r][c1]+grid[r][c2]
+
+            res=0
+            for c1_d in [-1,0,1]:
+                for c2_d in [-1,0,1]:
+                    res=max(res,dfs(r+1,c1+c1_d,c2+c2_d))
+
+            cache[(r,c1,c2)]=res+grid[r][c1]+grid[r][c2]
+            return cache[(r,c1,c2)]
+        return dfs(0,0,col-1)
+
+
+
+        
