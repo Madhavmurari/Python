5cef5ff86ae3382fe7a98ea33d809ce82f8f0ab2
Madhav Murari <143632752+Madhavmurari@users.noreply.github.com>
2024-10-25 07:37:53 +0530

    Time: 1146 ms (17.20%), Space: 18.7 MB (28.12%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,12 +1,30 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        n=len(coins)
-        dp=[float('inf') for i in range(amount+1)]
-        dp[0]=0
-        for coin in coins:
-            for amo in range(coin,amount+1):
-                dp[amo]=min(dp[amo],dp[amo-coin]+1)
-        return dp[amount] if dp[amount]!=float('inf') else -1
+        n = len(coins)
+        dp = [[float('inf')] * (amount + 1) for _ in range(n + 1)]
+        
+        for i in range(n + 1):
+            dp[i][0] = 0  # 0 coins needed to make amount 0
+
+        for i in range(1, n + 1):
+            for j in range(amount + 1):
+                dp[i][j] = dp[i-1][j]  # Exclude the current coin
+                
+                if coins[i-1] <= j:
+                    dp[i][j] = min(dp[i][j], 1 + dp[i][j - coins[i-1]])  # Include the current coin
+
+        result = dp[n][amount]
+        return result if result != float('inf') else -1
+
+
+
+
+        # dp=[0]+[float('inf')]*(amount+1)  ## dp[0]=0
+        
+        # for coin in coins:
+        #     for i in range(coin,amount+1):
+        #         dp[i]=min(dp[i],dp[i-coin]+1)
+        # return dp[amount] if dp[amount]!=float('inf') else -1
 
         
         
