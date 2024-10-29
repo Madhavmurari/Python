76c39e46b11925d35ff7d314837c174f2c0569b1
Madhav Murari <143632752+Madhavmurari@users.noreply.github.com>
2024-10-29 15:21:22 +0530

    Time: 370 ms (94.88%), Space: 16.7 MB (93.97%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,14 +1,25 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        # m,n=len(text1),len(text2)
+
+        # dp=[[0]*(n+1) for _ in range(m+1)]
+        # for i in range(1,m+1):
+        #     for j in range(1,n+1):
+        #         if text1[i-1]==text2[j-1]:
+        #             dp[i][j]=1+dp[i-1][j-1]
+        #         else:
+        #             dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+        # return dp[m][n]
+
         m,n=len(text1),len(text2)
 
-        dp=[[0]*(n+1) for _ in range(m+1)]
-
-        
+        prev=[0]*(n+1)
         for i in range(1,m+1):
+            curr=[0]*(n+1)
             for j in range(1,n+1):
                 if text1[i-1]==text2[j-1]:
-                    dp[i][j]=1+dp[i-1][j-1]
+                    curr[j]=1+prev[j-1]
                 else:
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1])
-        return dp[m][n]
+                    curr[j]=max(prev[j],curr[j-1])
+            prev=curr[:]
+        return prev[n]
