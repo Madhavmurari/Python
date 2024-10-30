f1efe15a34a819ad3c3871e1b8ef9ce50735c424
Madhav Murari <143632752+Madhavmurari@users.noreply.github.com>
2024-10-31 00:45:22 +0530

    Time: 259 ms (86.12%), Space: 16.9 MB (88.73%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,17 +1,40 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
-        cashe={}
+        m,n=len(s),len(t)
 
-        def dfs(i,j):
-            if j==len(t):
-                return 1
-            if i==len(s):
-                return 0
-            if (i,j) in cashe:
-                return cashe[(i,j)]
-            if s[i]==t[j]:
-                cashe[(i,j)] = dfs(i+1,j+1) + dfs(i+1,j)
-            else:
-                cashe[(i,j)] = dfs(i+1,j)
-            return cashe[(i,j)]
-        return dfs(0,0)
+        # dp=[[0]*(n+1) for i in range(m+1)]
+
+        # for i in range(m+1):
+        #     dp[i][0]=1
+        
+        # for i in range(1,m+1):
+        #     for j in range(1,n+1):
+        #         if s[i-1]==t[j-1]:
+        #             dp[i][j]=dp[i-1][j-1]+dp[i-1][j]
+        #         else:
+        #             dp[i][j]=dp[i-1][j]
+        # return dp[m][n]
+
+#########################################################################        
+        # prev=[0]*(n+1)
+        # prev[0]=1
+        # for i in range(1,m+1):
+        #     curr=[0]*(n+1)
+        #     curr[0]=1
+        #     for j in range(1,n+1):
+        #         if s[i-1]==t[j-1]:
+        #             curr[j]=prev[j-1]+prev[j]
+        #         else:
+        #             curr[j]=prev[j]
+        #     prev=curr[:]
+        # return prev[n]
+
+########################################################################
+        prev=[0]*(n+1)
+        for i in range(1,m+1):
+            prev[0]=1
+            for j in range(n,-1,-1):
+                if s[i-1]==t[j-1]:
+                    prev[j]=prev[j-1]+prev[j]
+                
+        return prev[n]
