3580c14441a9808648b9043a0a07a91fe8c0ed13
Madhav Murari <143632752+Madhavmurari@users.noreply.github.com>
2024-10-31 10:32:46 +0530

    Time: 48 ms (81.95%), Space: 16.7 MB (96.30%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,19 +1,35 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        n=len(word1)
-        m=len(word2)
-        dp=[[float("inf")]*(m+1) for i in range(n+1)]
+        m,n=len(word1),len(word2)
 
-        for i in range(m+1):  #make a table and fill the boundary value by length of decraeging order
-            dp[n][i]=m-i
+        # dp=[[0]*(n+1) for i in range(m+1)]
 
-        for i in range(n+1):
-            dp[i][m]=n-i
-        
-        for i in range(n-1,-1,-1):
-            for j in range(m-1,-1,-1):
-                if word1[i]==word2[j]:
-                    dp[i][j]=dp[i+1][j+1]
+        # for i in range(m+1):
+        #     dp[i][0]=i
+        # for j in range(n+1):
+        #     dp[0][j]=j
+
+        # for i in range(1,m+1):
+        #     for j in range(1,n+1):
+        #         if word1[i-1]==word2[j-1]:
+        #             dp[i][j]=dp[i-1][j-1]
+        #         else:
+        #             dp[i][j]=1+min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])
+        # return dp[m][n]
+#######################################################################
+
+        prev=[0]*(n+1)
+        for j in range(n+1):
+            prev[j]=j
+
+        for i in range(1,m+1):
+            curr=[0]*(n+1)
+            curr[0]=i
+            for j in range(1,n+1):
+                if word1[i-1]==word2[j-1]:
+                    curr[j]=prev[j-1]
                 else:
-                    dp[i][j] = 1 + min(dp[i][j+1],dp[i+1][j],dp[i+1][j+1])
-        return dp[0][0]
+                    curr[j]=1+min(prev[j],curr[j-1],prev[j-1])
+            prev=curr[:]
+
+        return prev[n]
