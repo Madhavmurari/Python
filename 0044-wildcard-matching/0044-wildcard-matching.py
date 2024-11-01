e58f6155c5633a5bf07fc55f94d5857adad29a82
Madhav Murari <143632752+Madhavmurari@users.noreply.github.com>
2024-11-01 10:35:34 +0530

    Time: 300 ms (65.84%), Space: 16.6 MB (82.45%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.py
+++ 0044-wildcard-matching/0044-wildcard-matching.py
@@ -1,21 +1,40 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         m,n=len(s),len(p)
+        prev=[False]*(n+1)
+        prev[0]=True
 
-        dp=[[False]*(n+1) for _ in range(m+1)]
-        dp[0][0]=True
-
-
-        for i in range(1,n+1):
-            if p[i-1]=="*":
-                dp[0][i]=dp[0][i-1]
+        for j in range(1,n+1):
+            if p[j-1]=='*':
+                prev[j]=prev[j-1]
+            
         
         for i in range(1,m+1):
+            curr=[False]*(n+1)
             for j in range(1,n+1):
-                if p[j-1]=="*":
-                    dp[i][j]=dp[i-1][j] or dp[i][j-1]
-                elif p[j-1]=='?' or s[i-1]==p[j-1]:
-                    dp[i][j]=dp[i-1][j-1]
+                if s[i-1]==p[j-1] or p[j-1]=='?':
+                    curr[j]=prev[j-1]
+                elif p[j-1]=='*':
+                    curr[j]=prev[j] or curr[j-1]
+            prev=curr
+            
+        return prev[n]
 
-        return dp[m][n]
 
+        # dp=[[False]*(n+1) for i in range(m+1)]
+
+        # dp[0][0]=True
+
+        # for j in range(1,n+1):
+        #     if p[j-1]=='*':
+        #         dp[0][j]=dp[0][j-1]
+            
+        
+        # for i in range(1,m+1):
+        #     for j in range(1,n+1):
+        #         if s[i-1]==p[j-1] or s[i-1]=='?':
+        #             dp[i][j]=dp[i-1][j-1]
+        #         elif p[j-1]=='*':
+        #             dp[i][j]=dp[i-1][j] or dp[i][j-1]
+        # return dp[m][n]
+        
